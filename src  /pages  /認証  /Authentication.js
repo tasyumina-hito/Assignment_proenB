0a6edbb6ad57d9692a23cf29dc7f803439af944d
tasyumina-hito <48035533+tasyumina-hito@users.noreply.github.com>
2022-07-26 14:17:01 +0900

    Update and rename src /pages /認証 /Authentication.js to src  /pages  /認証  /Authentication.js

--- src  /pages  /認証  /Authentication.js
+++ src  /pages  /認証  /Authentication.js
@@ -52,7 +52,7 @@ const Authentication = () => {
             align="center"
             sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem' }}
         >
-            <GoogleOAuthProvider clientId="process.env.">
+            <GoogleOAuthProvider clientId="process.env.REACT_APP_CLIENT_ID">
                 <GoogleLogin align="center"
                     onSuccess={credentialResponse => {
                         console.log(credentialResponse);
